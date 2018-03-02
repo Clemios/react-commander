a46cf25a5c03805ff3c9a263430c08b8ee17e31d
Clément Fleury <clement.fleury@morning.com>
2018-03-02 17:45:12 +0100

    base layout

--- app/App.js
+++ app/App.js
@@ -2,10 +2,17 @@ import React, { Component } from 'react';
 import { Switch, Route, Link } from 'react-router-dom';
 import { hot } from 'react-hot-loader';
 
+import { Sidebar, Segment, Button, Menu, Image, Icon, Header, Container } from 'semantic-ui-react'
+
 import Homus from './Homus';
 import Requettus from './Requettus';
 
 class App extends Component {
+
+  state = { visible: true }
+
+  toggleVisibility = () => this.setState({ visible: !this.state.visible })
+
   renderContent = () => (
     <Switch>
       <Route exact path="/" component={Homus} />
@@ -14,15 +21,47 @@ class App extends Component {
   )
 
   render() {
-    console.log(logo);
+    const { visible } = this.state
     return (
       <div className="App">
-        <header className="App-header">
-          <Link to="/"><button style={{ float: 'left' }}>Home</button></Link>
-          <Link to="/requests"><button style={{ float: 'right' }}>Requests</button></Link>
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        {this.renderContent()}
+        <Menu attached inverted className="menu-top">
+          <Container fluid>
+            <Menu.Item as="a" header>
+              <span>HEY</span>
+            </Menu.Item>
+            <Menu.Menu position="right">
+                <Menu.Item  as="a" href="/"><Icon name="power" />TITLE</Menu.Item>
+            </Menu.Menu>
+          </Container>
+        </Menu>
+        <Container fluid>
+        <Sidebar.Pushable as={Segment}>
+          <Sidebar as={Menu} animation='push' width='thin' visible={visible} icon='labeled' vertical inverted>
+            <Menu.Item name='home'>
+              <Icon name='home' />
+              Home
+            </Menu.Item>
+            <Menu.Item name='gamepad'>
+              <Icon name='gamepad' />
+              Games
+            </Menu.Item>
+            <Menu.Item name='camera'>
+              <Icon name='camera' />
+              Channels
+            </Menu.Item>
+          </Sidebar>
+          <Sidebar.Pusher>
+            <Segment basic>
+              <header className="App-header">
+                <Link to="/"><button style={{ float: 'left' }}>Home</button></Link>
+                <Link to="/requests"><button style={{ float: 'right' }}>Requests</button></Link>
+                <h1 className="App-title">Welcome to React</h1>
+              </header>
+              {this.renderContent()}
+            </Segment>
+          </Sidebar.Pusher>
+        </Sidebar.Pushable>
+        </Container>
       </div>
     );
   }
